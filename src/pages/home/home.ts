8576f67f3aeee7a5ecd931584955907d68d926ba
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2018-04-20 12:59:13 +0530

    button to view profile page added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {NewExpensePage} from "../new-expense/new-expense";
 import {ExpenseService} from "../../services/expense.services";
+import { GooglePlus } from '@ionic-native/google-plus';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  userProfile: any = null;
+
+  goToProfile():void {
+    this.navCtrl.push('ProfilePage');
+  }
 
   expenses: {title: string, amount: number}[] = [];
   constructor(public navCtrl: NavController, private expenseService : ExpenseService) {
