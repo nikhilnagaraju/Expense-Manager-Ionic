91223de5746d65eb8de6f8e64ae2c8941308d52f
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2018-04-20 12:52:38 +0530

    Redirect to login page if not authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import {LoginPage} from "../pages/login/login"
   ;
+import {AngularFireAuth} from "angularfire2/auth";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any;
 
-  constructor(app: App, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(app: App, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, afAuth: AngularFireAuth) {
+
+    afAuth.authState.subscribe( user => {
+      if (user){
+        this.rootPage = HomePage;
+      } else {
+        this.rootPage = 'LoginPage';
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
