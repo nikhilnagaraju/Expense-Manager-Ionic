39b30838721318b35f9d5abb4b3c88dffd5dee0c
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2018-04-20 12:56:11 +0530

    Profile page added to display basic info

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,25 @@
+import { AuthProvider } from './../../providers/auth/auth';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html'
+})
+export class ProfilePage {
+  public userProfile: any;
+  public userIdinfo: string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public authProvider: AuthProvider
+  ) {}
+
+  ionViewDidLoad() {
+    this.userProfile = this.authProvider.getUser();
+    let simple = JSON.parse(JSON.stringify(this.authProvider.getUser()));
+    this.userIdinfo = simple["uid"];
+  }
+}
